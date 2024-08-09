98f781c491aa997a6238895f494f028be8ac2429
Priscila <priscila.campoos@yahoo.com.br>
2024-08-09 18:32:28 +0100

    test: add new test in  HeroTest

--- src/Test/HeroTest.java
+++ src/Test/HeroTest.java
@@ -1,17 +1,25 @@
 package Test;
 
+import Entities.Npc;
+import Entities.Receptionist;
 import Entities.SalesRepresentative;
 import Test.TestHelper.HeroCreator;
+import Test.TestHelper.NpcCreator;
 
 public class HeroTest {
 
     public static void main(String[] args) {
-        SalesRepresentative hero = HeroCreator.createSalesRepresentative();
 
+
+        /*
+        SalesRepresentative hero = HeroCreator.createSalesRepresentative();
         hero.showDetails();
         hero.usePotion();
-        hero.showDetails();
+        hero.showDetails();*/
 
+        Receptionist receptionist = HeroCreator.createReceptionist();
+        receptionist.showDetails();
+        receptionist.attack(NpcCreator.createChallenge2());
 
     }
 }
