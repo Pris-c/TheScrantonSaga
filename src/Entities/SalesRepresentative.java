51f57d6675260f0bade9d1bee157b412837b76d6
Priscila <priscila.campoos@yahoo.com.br>
2024-08-10 11:54:27 +0100

    feat: implement method attack in class SalesRepresentative

--- src/Entities/SalesRepresentative.java
+++ src/Entities/SalesRepresentative.java
@@ -1,9 +1,13 @@
 package Entities;
 
+import Items.CombatConsumable;
 import Items.Consumable;
 import Items.Weapon;
+import Util.Util;
 
 import java.util.ArrayList;
+
+import static Util.Util.readAndValidateInput;
 
 public class SalesRepresentative extends Hero{
 
@@ -19,7 +23,110 @@ public class SalesRepresentative extends Hero{
 
     @Override
     public boolean attack(Npc enemy) {
-        // Builds a combat and returns yes if the hero wins
+        // Cavaleiro
+
+        int decrement = 0;
+        boolean next;
+        boolean specialAttack;
+        boolean endOfFight = false;
+        int option;
+        int enemyStrength;
+        specialAttack = true;
+
+        do{
+
+            // Enemy Attack
+            enemyStrength = (int) Math.round(enemy.strength * 0.8);
+            if (this.decrementHp(enemyStrength) <= 0){
+                System.out.println("Opss, o inimigo acabou com você!");
+                return false;
+            }
+            System.out.println("Ouch! Você foi atingido! Agora só te restam " + this.getHp() + " HP");
+
+            //Hero Attack
+            do{
+
+                next = true;
+                if (specialAttack) {
+                    String message = "Escolha o tipo de ataque:\n1 - Utilizar um Consumível de Combate\n2 - Ataque comum \n3 - Ataque Especial";
+                    option = Util.readAndValidateInput(message, 1, 3);
+                } else {
+                    String message = "Escolha o tipo de ataque:\n1 - Utilizar um Consumível de Combate\n2 - Ataque comum";
+                    option = Util.readAndValidateInput(message, 1, 2);
+                }
+
+                switch (option) {
+                    case 1:
+                        int instantAttack = instantAttack();
+                        if (instantAttack != -1){
+                            decrement = instantAttack;
+                        } else {
+                            next = false;
+                        }
+                        break;
+                    case 2:
+                        decrement = this.strength + this.mainWeapon.getStandardAttack();
+                        break;
+                    case 3:
+                        decrement = this.strength + this.mainWeapon.getSpecialAttack();
+                        specialAttack = false;
+                        break;
+                }
+            } while (!next);
+
+            // Decrement enemy HP
+            if (enemy.decrementHp(decrement) <= 0){
+                System.out.println("Yeah! Você derrotou o inimigo!");
+                return true;
+            }
+            System.out.println("Boa! Você acertou o inimigo! Agora só restam " + enemy.getHp() + " HP");
+
+
+        } while (!endOfFight);
+
         return false;
     }
+
+
+    private int instantAttack() {
+        // Shows CombatConsumables in inventory
+        if (super.inventory.isEmpty()) {
+            System.out.println("Desculpe, o seu inventário está vazio!");
+            return -1;
+        }
+
+        // Filter CombatConsumable in inventory
+        ArrayList<CombatConsumable> consumables =
+                new ArrayList<>(inventory.stream().
+                        filter(i -> i instanceof CombatConsumable)
+                        .map(i -> (CombatConsumable) i)
+                        .toList()
+                );
+
+        if (consumables.isEmpty()) {
+            System.out.println("Desculpe, você não tem nenhum Consumível de Combate disponível!");
+            return -1;
+        }
+
+        // Show available consumables
+        System.out.println("Consumíveis disponíveis: ");
+        for (int i = 0; i < consumables.size(); i++) {
+            System.out.println("# " + (i + 1));
+            consumables.get(i).showDetails();
+            System.out.println();
+        }
+
+        // Read and validate an option
+        int option = readAndValidateInput("Escolha o item a utilizar:", 0, consumables.size());
+        if (option != 0) {
+            CombatConsumable consumable = consumables.get(option-1);
+            int decrement = consumable.getInstantAttack();
+            inventory.remove(consumable);
+            return decrement;
+        }
+        return -1;
+    }
+
+
+
 }
