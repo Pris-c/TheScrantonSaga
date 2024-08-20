c99789504a12deebc51c45729ebfa77c7d15387b
Priscila <priscila.campoos@yahoo.com.br>
2024-08-20 16:43:24 +0100

    test: create GameTest class

---
+++ src/Test/GameTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import Game.Game;
+
+public class GameTest {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        game.createHero().showDetails();
+
+    }
+}
