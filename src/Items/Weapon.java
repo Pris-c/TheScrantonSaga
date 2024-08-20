1e7e5131613cd07e01fa53c3ed044cf2ecd3764b
Priscila <priscila.campoos@yahoo.com.br>
2024-08-20 18:29:28 +0100

    feat: add standarAttackddescription and specialAttackdescription attributes to class Weapon

--- src/Items/Weapon.java
+++ src/Items/Weapon.java
@@ -2,26 +2,36 @@ package Items;
 
 import java.util.ArrayList;
 
-public class Weapon extends ItemHero{
+public class Weapon extends ItemHero {
 
     private int standardAttack;
     private int specialAttack;
-    private String description;
+    private String standarAttackddescription;
+    private String specialAttackdescription;
 
-    public Weapon(String name, int price, ArrayList<String> allowedTo, int standardAttack, int specialAttack, String description) {
+    public Weapon(String name, int price, ArrayList<String> allowedTo, int standardAttack, int specialAttack, String standarAttackddescription, String specialAttackdescription) {
         super(name, price, allowedTo);
         this.standardAttack = standardAttack;
         this.specialAttack = specialAttack;
-        this.description = description;
+        this.standarAttackddescription = standarAttackddescription;
+        this.specialAttackdescription = specialAttackdescription;
     }
 
     @Override
     public void showDetails() {
         System.out.println("** Arma: " + super.name + " **");
-        System.out.println("Descrição: " + this.description);
+        System.out.println("Descrição: " + this.standarAttackddescription);
         System.out.println("Ataque comum: " + this.standardAttack);
         System.out.println("Ataque especial: " + this.specialAttack);
         super.showDetails();
+    }
+
+    public String getStandarAttackddescription() {
+        return standarAttackddescription;
+    }
+
+    public String getSpecialAttackdescription() {
+        return specialAttackdescription;
     }
 
     public int getStandardAttack() {
