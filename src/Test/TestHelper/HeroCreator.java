f9a856f2df972f7956460e9ee815a90d1a2cc407
Priscila <priscila.campoos@yahoo.com.br>
2024-08-20 16:42:10 +0100

    fix: adapt methods to new Heroes constructors

--- src/Test/TestHelper/HeroCreator.java
+++ src/Test/TestHelper/HeroCreator.java
@@ -3,43 +3,18 @@ package Test.TestHelper;
 import Entities.Intern;
 import Entities.Receptionist;
 import Entities.SalesRepresentative;
-import Items.Consumable;
-import Items.Potion;
-import Items.Weapon;
-
-import java.util.ArrayList;
 
 public class HeroCreator {
 
     public static Intern createIntern() {
-        WeaponCreator WeaponCreator;
-        Weapon weapon = Test.TestHelper.WeaponCreator.createSickRelativeExcuse();
-        ArrayList<Consumable> inventory = new ArrayList<>();
-        inventory.add(PotionCreator.coffee());
-        inventory.add(PotionCreator.cerealBar());
-        inventory.add(CombatConsumableCreator.createRyanHowardHelp());
-        inventory.add(CombatConsumableCreator.createStanleyHudsonHelp());
-
-        return new Intern("José", 100, 10, 50, weapon, new ArrayList<>(inventory));
+        return new Intern("José", 100, 10, 50);
     }
 
     public static SalesRepresentative createSalesRepresentative() {
-        Weapon weapon = WeaponCreator.createSecretThreat();
-        ArrayList<Consumable> inventory = new ArrayList<>();
-        inventory.add(PotionCreator.eggSalad());
-        inventory.add(PotionCreator.soda());
-        inventory.add(CombatConsumableCreator.createDwightSchruteHelp());
-        inventory.add(CombatConsumableCreator.createAngelaMartinHelp());
-        return new SalesRepresentative("Jéssica", 120, 12, 75, weapon, new ArrayList<>(inventory));
+        return new SalesRepresentative("Jéssica", 120, 12, 75);
     }
 
     public static Receptionist createReceptionist() {
-        Weapon weapon = WeaponCreator.createCEOFriend();
-        ArrayList<Consumable> inventory = new ArrayList<>();
-        inventory.add(PotionCreator.tunaSandwich());
-        inventory.add(PotionCreator.fullMeal());
-        inventory.add(CombatConsumableCreator.createPamBeeslyHelp());
-        inventory.add(CombatConsumableCreator.createJimHalpertHelp());
-        return new Receptionist("Diego", 110, 11, 60, weapon, new ArrayList<>(inventory));
+        return new Receptionist("Diego", 110, 11, 60);
     }
 }
