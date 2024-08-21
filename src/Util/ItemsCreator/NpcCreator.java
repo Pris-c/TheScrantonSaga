4e99ac066aeb7a14c8d9e36b4660f6f6b433c296
Priscila <priscila.campoos@yahoo.com.br>
2024-08-21 14:16:53 +0100

    fix: update Npc creation to new constructor

--- src/Util/ItemsCreator/NpcCreator.java
+++ src/Util/ItemsCreator/NpcCreator.java
@@ -5,39 +5,32 @@ import Entities.Npc;
 public class NpcCreator {
 
     public static Npc createMichaelLunch() {
-        return new Npc("Pedido Especial do Michael", 30, 15, 20,
-                "Michael esqueceu de trazer o almoço novamente e você foi encarregado de buscar. Cuidado, ele pode ser exigente!");
+        return new Npc("Pedido Especial do Michael", 30, 15, 20);
     }
 
     public static Npc createFixingPrinter() {
-        return new Npc("O Eterno Conserto da Impressora", 40, 20, 25,
-                "A impressora do escritório quebrou de novo, e todos estão esperando por você para consertá-la. Este é um trabalho delicado!");
+        return new Npc("O Eterno Conserto da Impressora", 40, 20, 25);
 
     }
 
     public static Npc createRecoveringCustomer() {
-        return new Npc("Missão: Reconquista de Cliente", 50, 25, 30,
-                "Um cliente importante está pensando em cancelar o contrato. Você precisa usar todo o seu charme e persuasão para mantê-lo.");
+        return new Npc("Missão: Reconquista de Cliente", 50, 25, 30);
     }
 
     public static Npc createParkourChallenge() {
-        return new Npc("Caos de Parkour no Escritório", 60, 30, 35,
-                "Dwight está fazendo parkour no escritório novamente. Mantenha o controle para não deixar tudo virar um caos.");
+        return new Npc("Caos de Parkour no Escritório", 60, 30, 35);
     }
 
     public static Npc createRhMeeting() {
-        return new Npc("Tortura na Sala do RH", 70, 35, 40,
-                "Toby convocou mais uma reunião. Você precisará de toda a sua paciência e diplomacia para sobreviver a esta.");
+        return new Npc("Tortura na Sala do RH", 70, 35, 40);
     }
 
     public static Npc createBasketGame() {
-        return new Npc("Desafio das Quadras: Escritório vs. Armazém", 80, 40, 50,
-                "É hora do jogo anual de basquete contra o pessoal do armazém. Prepare-se para um confronto acirrado!");
+        return new Npc("Desafio das Quadras: Escritório vs. Armazém", 80, 40, 50);
     }
 
     public static Npc createChristmasParty() {
-        return  new Npc("A Temida Festa de Natal", 90, 45, 60,
-                "O escritório confiou a você a organização da festa de Natal. Mas cuidado, satisfazer todos os gostos é uma tarefa heróica!");
+        return  new Npc("A Temida Festa de Natal", 90, 45, 60);
     }
 
 }
