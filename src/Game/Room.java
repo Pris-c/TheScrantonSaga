d4dc4e82b5b0a4ea7c4124f61754ecc0851d57a2
Priscila <priscila.campoos@yahoo.com.br>
2024-08-21 13:34:28 +0100

    feat: update method lunchChallenge

--- src/Game/Room.java
+++ src/Game/Room.java
@@ -13,7 +13,16 @@ public class Room {
                 "\nO problema? Michael é indeciso, e suas demandas são cada vez mais absurdas." +
                 "\nVocê precisa agir rápido antes que ele mude de ideia novamente." +
                 "\nSerá que você consegue realizar essa tarefa sem perder a cabeça?");
-        return hero.attack(NpcCreator.createMichaelLunch());
+
+        boolean heroWin = hero.attack(NpcCreator.createMichaelLunch());
+
+        if (heroWin){
+            System.out.println("Yeah! Você se livrou dessa! \nO Andy ouviu a conversa e logo se voluntariou para ir comprar o almoço do Michael! \nEle não poderia perder essa oportunidade de ganhar créditos com o chefe!");
+        } else {
+            System.out.println("Oh não! Você trouxe tiramisú de sobremesa e o Michael se engasgou com o chocolate em pó! \nEle não quer que a vida dele volte a correr riscos tão grandes, e por isso você foi demitido!");
+        }
+
+        return heroWin;
     }
 
 
