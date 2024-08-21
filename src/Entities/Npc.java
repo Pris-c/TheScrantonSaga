8a5e1c76be1a5079e947f1896dcbe1f71c9afbbb
Priscila <priscila.campoos@yahoo.com.br>
2024-08-21 14:16:03 +0100

    refactor: remove attribute description from Npc class

--- src/Entities/Npc.java
+++ src/Entities/Npc.java
@@ -2,21 +2,14 @@ package Entities;
 
 public class Npc extends Entity {
 
-    private String description;
 
-    public Npc(String name, int maxHp, int strength, int gold, String description) {
+    public Npc(String name, int maxHp, int strength, int gold) {
         super(name, maxHp, strength, gold);
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     @Override
     public void showDetails() {
         System.out.println("\n ** Desafio: " + super.name + " **");
-        System.out.println("Description: " + this.description);
         super.showDetails();
     }
 }
