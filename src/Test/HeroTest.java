ecbc708280362bc96a22f34bfef95bc160776043
Priscila <priscila.campoos@yahoo.com.br>
2024-08-22 12:04:23 +0100

    test: add test to Entities classes methods showDetails

--- src/Test/HeroTest.java
+++ src/Test/HeroTest.java
@@ -5,30 +5,62 @@ import Entities.Npc;
 import Entities.Receptionist;
 import Entities.SalesRepresentative;
 import Test.TestHelper.HeroCreator;
-import Test.TestHelper.NpcCreator;
+import Util.ItemsCreator.NpcCreator;
 
 public class HeroTest {
 
     public static void main(String[] args) {
 
-
-/*        SalesRepresentative hero = HeroCreator.createSalesRepresentative();
-        hero.showDetails();
-        hero.usePotion();
-        hero.showDetails();*/
-
-/*        Receptionist receptionist = HeroCreator.createReceptionist();
-        receptionist.showDetails();
-        receptionist.attack(NpcCreator.createChallenge2());*/
-
-/*        SalesRepresentative salesRepresentative = HeroCreator.createSalesRepresentative();
-        salesRepresentative.showDetails();
-        salesRepresentative.attack(NpcCreator.createChallenge3());*/
-
+        System.out.println(" ----------- TESTING SHOW DETAILS -----------\n");
+        System.out.println(" ----------- HEROES CLASSES -----------\n");
+        Receptionist receptionist = HeroCreator.createReceptionist();
+        SalesRepresentative salesRepresentative = HeroCreator.createSalesRepresentative();
         Intern intern = HeroCreator.createIntern();
+        receptionist.showDetails();
+        salesRepresentative.showDetails();
+        intern.showDetails();
+        System.out.println(" ----------- CHALLENGES -----------\n");
+        Npc basketGame = NpcCreator.createBasketGame();
+        Npc christmasParty = NpcCreator.createChristmasParty();
+        Npc rhMeeting = NpcCreator.createRhMeeting();
+        Npc fixingPrinter = NpcCreator.createFixingPrinter();
+        Npc michaelLunch = NpcCreator.createMichaelLunch();
+        Npc parkourChallenge = NpcCreator.createParkourChallenge();
+        Npc recoveringCustomer = NpcCreator.createRecoveringCustomer();
+        basketGame.showDetails();
+        christmasParty.showDetails();
+        rhMeeting.showDetails();
+        fixingPrinter.showDetails();
+        michaelLunch.showDetails();
+        parkourChallenge.showDetails();
+        recoveringCustomer.showDetails();
+
+
+/*
+
+        System.out.println(receptionist.getClass().getSimpleName());
+        receptionist.showDetails();
+        receptionist.attack(NpcCreator.createMichaelLunch());
+
+
+        salesRepresentative.showDetails();
+        salesRepresentative.attack(NpcCreator.createMichaelLunch());
+
+
         intern.showDetails();
         System.out.println();
-        intern.attack(NpcCreator.createChallenge1());
+        if (intern.attack(NpcCreator.createMichaelLunch())){
+            System.out.println("Parabens, voce venceu");
+        } else{
+            System.out.println("Fim de jogo");
+        }
+
+        Npc npc = NpcCreator.createBasketGame();
+        npc.showDetails();
+
+
+        Weapon weapon = WeaponCreator.createSecretThreat();
+        weapon.showDetails();*/
 
     }
 }
