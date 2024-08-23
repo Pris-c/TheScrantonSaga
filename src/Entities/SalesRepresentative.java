2d8a42a784d6db1f6e29144415eea36003ca5aed
Priscila <priscila.campoos@yahoo.com.br>
2024-08-23 16:02:39 +0100

    refactor: update method attack and create method printScoreboard

--- src/Entities/SalesRepresentative.java
+++ src/Entities/SalesRepresentative.java
@@ -8,8 +8,7 @@ import Util.Util;
 import java.util.ArrayList;
 import java.util.List;
 
-import static Util.Util.detailFormat;
-import static Util.Util.singleTextFormat;
+import static Util.Util.*;
 
 public class SalesRepresentative extends Hero {
 
@@ -20,15 +19,14 @@ public class SalesRepresentative extends Hero {
     @Override
     public void showDetails() {
         System.out.println("+-----------------------------------------------------------------------------+");
-        System.out.printf(singleTextFormat, "                  DETALHES REPRESENTANTE VENDAS");
+        System.out.printf(largeTextFormat, "                  DETALHES REPRESENTANTE VENDAS");
         System.out.println("+-----------------------------------------------------------------------------+");
-        System.out.printf(detailFormat, "Nome", super.name);
+        System.out.printf(largeDetailFormat, "Nome", super.name);
         super.showDetails();
     }
 
     @Override
     public boolean attack(Npc enemy) {
-        // Cavaleiro
 
         int decrement = 0;
         boolean next;
@@ -39,26 +37,28 @@ public class SalesRepresentative extends Hero {
         specialAttack = true;
 
         do {
+            printScoreboard(enemy);
 
             // Enemy Attack
             enemyStrength = (int) Math.round(enemy.strength * 0.8);
             if (this.decrementHp(enemyStrength) <= 0) {
                 return false;
             }
-            System.out.println("Michal acha que você ainda não se esforçou o suficiente! Agora só te restam " + this.getHp() + " HP");
+            System.out.println("Você não agiu rápido o suficiente e perdeu " + enemy.getStrength() + " HP.\n");
 
-            //Hero Attack
             do {
+                printScoreboard(enemy);
 
                 next = true;
                 if (specialAttack) {
-                    String message = "Escolha como lidar com essa situação::\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão \n3 - Utilizar seus próprios meios com energia extra";
+                    String message = "\nEscolha como lidar com essa situação:\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão \n3 - Utilizar seus próprios meios com energia extra";
                     option = Util.readAndValidateInput(message, 1, 3);
                 } else {
-                    String message = "Escolha como lidar com essa situação::\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão";
+                    String message = "Escolha como lidar com essa situação:\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão";
                     option = Util.readAndValidateInput(message, 1, 2);
                 }
 
+                cleanScreen();
                 switch (option) {
                     case 1:
                         int instantAttack = super.instantAttack();
@@ -70,26 +70,39 @@ public class SalesRepresentative extends Hero {
                         break;
                     case 2:
                         decrement = this.strength + this.mainWeapon.getStandardAttack();
-                        System.out.println(this.mainWeapon.getStandarAttackddescription());
                         break;
                     case 3:
                         decrement = this.strength + this.mainWeapon.getSpecialAttack();
-                        System.out.println(this.mainWeapon.getSpecialAttackdescription());
                         specialAttack = false;
                         break;
                 }
+                cleanScreen();
             } while (!next);
 
-            // Decrement enemy HP
             if (enemy.decrementHp(decrement) <= 0) {
                 return true;
             }
-            System.out.println("Boa! Você tornou as coisas mais fáceis! Faltam " + enemy.getHp() + " pontos para concluir a missão");
-
+            System.out.println("Boa! Você tornou as coisas mais fáceis!\n");
 
         } while (!endOfFight);
 
         return false;
     }
+
+    private void printScoreboard(Npc enemy) {
+        String headerFormat = "\t| %-16s | %-16s |\n";
+        String scoreFormat = "\t| %16d | %16d |\n";
+
+        String scoreboard =
+                "\t+------------------+------------------+\n" +
+                        "\t|           PLACAR                    |\n" +
+                        "\t+------------------+------------------+\n" +
+                        String.format(headerFormat, "Herói", "Missão") +
+                        "\t+------------------+------------------+\n" +
+                        String.format(scoreFormat, this.hp, enemy.hp) +
+                        "\t+------------------+------------------+\n\n";
+        System.out.println(scoreboard);
+    }
+
 
 }
