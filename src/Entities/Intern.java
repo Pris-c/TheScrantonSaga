cdf739986d1f48c6d4874245160cad09e780983e
Priscila <priscila.campoos@yahoo.com.br>
2024-08-23 15:41:32 +0100

    refactor: update method attack and create method printScoreboard

--- src/Entities/Intern.java
+++ src/Entities/Intern.java
@@ -8,8 +8,7 @@ import Util.Util;
 import java.util.ArrayList;
 import java.util.List;
 
-import static Util.Util.detailFormat;
-import static Util.Util.singleTextFormat;
+import static Util.Util.*;
 
 public class Intern extends Hero {
 
@@ -20,38 +19,36 @@ public class Intern extends Hero {
     @Override
     public void showDetails() {
         System.out.println("+-----------------------------------------------------------------------------+");
-        System.out.printf(singleTextFormat, "                           DETALHES ESTAGIÁRIO");
+        System.out.printf(largeTextFormat, "                           DETALHES ESTAGIÁRIO");
         System.out.println("+-----------------------------------------------------------------------------+");
-        System.out.printf(detailFormat, "Nome", super.name);
+        System.out.printf(largeDetailFormat, "Nome", super.name);
         super.showDetails();
     }
 
     @Override
     public boolean attack(Npc enemy) {
-        // Arqueiro
 
         boolean specialAttack = true;
         boolean endOfFight = false;
 
-        // turn
         do {
-
             int decrement = 0;
             boolean next;
             int option;
             int enemyStrength;
 
-            //Hero Attack
             do {
+                printScoreboard(enemy);
                 next = true;
                 if (specialAttack) {
-                    String message = "Escolha como lidar com essa situação:\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão \n3 - Utilizar seus próprios meios com energia extra";
+                    String message = "\nEscolha como lidar com essa situação:\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão \n3 - Utilizar seus próprios meios com energia extra";
                     option = Util.readAndValidateInput(message, 1, 3);
                 } else {
                     String message = "Escolha como lidar com essa situação:\n1 - Utilizar ajuda de um colega que te deve um favor.\n2 - Utilizar seus próprios meios para cumprir a missão";
                     option = Util.readAndValidateInput(message, 1, 2);
                 }
 
+                cleanScreen();
                 switch (option) {
                     case 1:
                         int instantAttack = super.instantAttack();
@@ -63,31 +60,47 @@ public class Intern extends Hero {
                         break;
                     case 2:
                         decrement = this.strength + this.mainWeapon.getStandardAttack();
-                        System.out.println(this.mainWeapon.getStandarAttackddescription());
+                        //TODO: REMOVE ATTRIBUTE System.out.println(this.mainWeapon.getStandarAttackddescription());
                         break;
                     case 3:
                         decrement = this.strength + this.mainWeapon.getSpecialAttack();
-                        System.out.println(this.mainWeapon.getSpecialAttackdescription());
+                        //TODO: REMOVE ATTRIBUTE System.out.println(this.mainWeapon.getSpecialAttackdescription());
                         specialAttack = false;
                         break;
                 }
+
+                cleanScreen();
             } while (!next);
 
-            // Decrement enemy HP
             if (enemy.decrementHp(decrement) <= 0) {
                 return true;
             }
-            System.out.println("Boa! Você tornou as coisas mais fáceis! Faltam " + enemy.getHp() + " pontos para concluir a missão");
+            System.out.println("Boa! Você tornou as coisas mais fáceis!!");
 
             // Enemy Attack
             enemyStrength = (int) Math.round(enemy.strength * 1.10);
             if (this.decrementHp(enemyStrength) <= 0) {
                 return false;
             }
-            System.out.println("Michal acha que você ainda não se esforçou o suficiente! Agora só te restam " + this.getHp() + " HP");
+            System.out.println("Mas você ainda não conseguiu concluir a missão e perdeu " + enemyStrength + " HP.\n");
 
         } while (!endOfFight);
         return false;
+    }
+
+    private void printScoreboard(Npc enemy) {
+        String headerFormat = "\t| %-16s | %-16s |\n";
+        String scoreFormat = "\t| %16d | %16d |\n";
+
+        String scoreboard =
+                "\t+------------------+------------------+\n" +
+                        "\t|           PLACAR                    |\n" +
+                        "\t+------------------+------------------+\n" +
+                        String.format(headerFormat, "Herói", "Missão") +
+                        "\t+------------------+------------------+\n" +
+                        String.format(scoreFormat, this.hp, enemy.hp) +
+                        "\t+------------------+------------------+\n\n";
+        System.out.println(scoreboard);
     }
 
 }
