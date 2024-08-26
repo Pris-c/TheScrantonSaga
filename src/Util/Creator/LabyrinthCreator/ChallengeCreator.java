0294a0a98585da5908c16f2f414c4f492b73adef
Priscila <priscila.campoos@yahoo.com.br>
2024-08-26 10:57:21 +0100

    feat: create methods to challenges 3 to 7

--- src/Util/Creator/LabyrinthCreator/ChallengeCreator.java
+++ src/Util/Creator/LabyrinthCreator/ChallengeCreator.java
@@ -10,7 +10,28 @@ public class ChallengeCreator {
     public Challenge createChallenge1(){
         return new Challenge(NpcCreator.createMichaelLunch(), challenge1Introduction, challenge1VictoryMessage, challenge1DefeatMessage);
     }
+
     public Challenge createChallenge2(){
         return new Challenge(NpcCreator.createFixingPrinter(), challenge2Introduction, challenge2VictoryMessage, challenge2DefeatMessage);
+    }
+
+    public Challenge createChallenge3(){
+        return new Challenge(NpcCreator.createRecoveringCustomer(), challenge3Introduction, challenge3VictoryMessage, challenge3DefeatMessage);
+    }
+
+    public Challenge createChallenge4(){
+        return new Challenge(NpcCreator.createParkourChallenge(), challenge4Introduction, challenge4VictoryMessage, challenge4DefeatMessage);
+    }
+
+    public Challenge createChallenge5(){
+        return new Challenge(NpcCreator.createRhMeeting(), challenge5Introduction, challenge5VictoryMessage, challenge5DefeatMessage);
+    }
+
+    public Challenge createChallenge6(){
+        return new Challenge(NpcCreator.createBasketGame(), challenge6Introduction, challenge6VictoryMessage, challenge6DefeatMessage);
+    }
+
+    public Challenge createChallenge7(){
+        return new Challenge(NpcCreator.createChristmasParty(), challenge7Introduction, challenge7VictoryMessage, challenge7DefeatMessage);
     }
 }
