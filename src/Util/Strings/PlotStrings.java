8762111ba51c2752949c6898721c3454b2577d64
Priscila <priscila.campoos@yahoo.com.br>
2024-08-26 10:54:23 +0100

    feat: add Challenges 3 to 7 Strings

--- src/Util/Strings/PlotStrings.java
+++ src/Util/Strings/PlotStrings.java
@@ -172,4 +172,213 @@ public class PlotStrings {
                 "  \\_______________________________________________________________/\n\n";
 
 
+
+
+    public static String challenge3Introduction =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Um cliente importante ameaçou levar seu negócio para outra      |\n" +
+            "|   empresa, e todos estão em pânico. A responsabilidade de salvar  |\n" +
+            "|   essa conta recai sobre você. É hora de mostrar suas habilidades |\n" +
+            "|   de persuasão e reconquistar o cliente antes que seja tarde      |\n" +
+            "|   demais. Você conseguirá trazer o cliente de volta, ou o         |\n" +
+            "|   escritório enfrentará uma crise?                                |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+    public static String challenge3VictoryMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Parabéns! Sua habilidade de persuasão impressionou o cliente,  |\n" +
+            "|   que decidiu permanecer com a Dunder Mifflin. O escritório       |\n" +
+            "|   inteiro está aliviado, e você foi reconhecido como o salvador   |\n" +
+            "|   do dia.                                                        |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+    public static String challenge3DefeatMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Infelizmente, o cliente não foi convencido e decidiu levar     |\n" +
+            "|   seu negócio para a concorrência. O escritório entrou em crise  |\n" +
+            "|   e perdeu a confiança em você.                                  |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+
+    public static String challenge4Introduction =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Dwight e Michael decidiram animar o dia com um pouco de        |\n" +
+            "|   Parkour! Agora, o escritório virou um campo de obstáculos      |\n" +
+            "|   improvisado. Sua missão é simples: sobreviver à loucura sem    |\n" +
+            "|   causar muitos danos ao escritório (ou a si mesmo). Mas cuidado,|\n" +
+            "|   a competição é acirrada! Está preparado para saltar sobre      |\n" +
+            "|   mesas e evitar cair em apuros?                                 |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+    public static String challenge4VictoryMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Incrível! Você demonstrou habilidades atléticas impressionantes|\n" +
+            "|   e se manteve ileso durante toda a brincadeira. Todos ficaram   |\n" +
+            "|   impressionados com sua destreza, e o escritório está em        |\n" +
+            "|   perfeito estado.                                               |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+    public static String challenge4DefeatMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Ouch! Sua tentativa de Parkour não saiu como planejado, e agora|\n" +
+            "|   há várias mesas derrubadas e documentos espalhados por todo o  |\n" +
+            "|   escritório. Michael está um pouco desapontado.                 |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+
+    public static String challenge5Introduction =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Chegou o dia da temida Reunião do RH, um evento conhecido      |\n" +
+            "|   por suas discussões acaloradas e debates intermináveis. O     |\n" +
+            "|   escritório está em frangalhos com uma lista de queixas: o      |\n" +
+            "|   Kelly reclamou da falta de ar-condicionado, o Dwight trouxe    |\n" +
+            "|   uma série de acusações sobre 'desperdício de recursos', e o    |\n" +
+            "|   Jim está frustrado com a quantidade excessiva de e-mails        |\n" +
+            "|   internos. Sua missão é atravessar esse campo minado de         |\n" +
+            "|   reclamações e resolver as questões de forma diplomática. Está  |\n" +
+            "|   preparado para ser o mediador entre as demandas extravagantes  |\n" +
+            "|   e as expectativas irreais do RH?                               |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+    public static String challenge5VictoryMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Você conseguiu transformar a reunião em uma verdadeira sessão   |\n" +
+            "|   de cinema para o escritório! Com suas habilidades de mediação  |\n" +
+            "|   e uma estratégia bem elaborada, convenceu o RH a exibir um     |\n" +
+            "|   filme clássico e proporcionar uma sessão de relaxamento para   |\n" +
+            "|   todos. As tensões se dissiparam, e o escritório saiu da        |\n" +
+            "|   reunião com uma nova energia. Todos elogiaram sua criatividade  |\n" +
+            "|   e habilidade para transformar um evento mais chato do trabalho  |\n" +
+            "|   em um momento de diversão e união.                             |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+    public static String challenge5DefeatMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   A tensão e o tédio da reunião foram demais para você...        |\n" +
+            "|   Depois de horas de discussões intermináveis, você não conseguiu|\n" +
+            "|   aguentar e acabou dormindo na sala. Quando acordou, todos já    |\n" +
+            "|   haviam ido embora, e a última coisa que você ouviu foi o RH    |\n" +
+            "|   encerrando seu contrato. Parece que esta foi sua última reunião |\n" +
+            "|   no escritório...                                               |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+
+    public static String challenge6Introduction =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   O evento mais aguardado do ano chegou: o jogo de basquete     |\n" +
+            "|   contra o Armazém. As rivalidades estão em alta, e a vitória   |\n" +
+            "|   significa mais do que apenas bragging rights. É hora de       |\n" +
+            "|   colocar suas habilidades à prova e levar seu time à glória.   |\n" +
+            "|   Mas cuidado, o time do Armazém joga duro! Você conseguirá      |\n" +
+            "|   liderar seu time à vitória?                                   |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+    public static String challenge6VictoryMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Vitória! Você liderou o time com habilidade e garantiu a       |\n" +
+            "|   glória para o escritório. O time do Armazém foi derrotado, e    |\n" +
+            "|   agora todos no escritório te veem como o herói do jogo.        |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+    public static String challenge6DefeatMessage =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   O jogo foi um desastre completo. O time do Armazém esmagou o    |\n" +
+            "|   seu, e o escritório está em frustração total. Michael não       |\n" +
+            "|   para de relembrar cada erro cometido e, como se não bastasse,   |\n" +
+            "|   você está sendo cobrado por cada falha. A derrota foi um golpe  |\n" +
+            "|   duro, e o escritório agora enfrenta um clima de desânimo e      |\n" +
+            "|   desconfiança. Infelizmente, a derrota neste desafio é o fim     |\n" +
+            "|   da sua jornada. O jogo acabou, e a próxima vez terá que esperar.|\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+
+    public static String challenge7Introduction =
+            "   _______________________________________________________________\n" +
+            "  /                                                               \\\n" +
+            " /                                                                 \\\n" +
+            "|   Finalmente, a Festa de Natal do escritório está se aproximando, |\n" +
+            "|   e você foi encarregado de organizar tudo. Mas nada é simples    |\n" +
+            "|   quando se trata do Dunder Mifflin. Entre pedidos impossíveis,  |\n" +
+            "|   orçamentos apertados e expectativas absurdas, você terá que    |\n" +
+            "|   manter todos felizes enquanto cria a festa perfeita. Será que  |\n" +
+            "|   você conseguirá transformar essa bagunça em uma celebração      |\n" +
+            "|   memorável?                                                     |\n" +
+            " \\                                                                 /\n" +
+            "  \\_______________________________________________________________/\n\n";
+
+
+     public static String challenge7VictoryMessage =
+             "   _______________________________________________________________\n" +
+             "  /                                                               \\\n" +
+             " /                                                                 \\\n" +
+             "|   A festa foi um sucesso absoluto! Você conseguiu transformar uma |\n" +
+             "|   situação caótica em uma celebração incrível. Todos no escritório |\n" +
+             "|   estão radiantes com a festa, e Michael não para de falar sobre  |\n" +
+             "|   como foi a melhor que o escritório já teve. Você superou todas  |\n" +
+             "|   as expectativas e conquistou o título de herói da festa.         |\n" +
+             "|   Parabéns, você venceu o jogo e terminou sua jornada com chave de |\n" +
+             "|   ouro!                                                            |\n" +
+             " \\                                                                 /\n" +
+             "  \\_______________________________________________________________/\n\n";
+
+     public static String challenge7DefeatMessage =
+             "   _______________________________________________________________\n" +
+             "  /                                                               \\\n" +
+             " /                                                                 \\\n" +
+             "|   A festa acabou sendo um desastre completo... A decoração estava  |\n" +
+             "|   fora do lugar, o buffet foi um fiasco e as reclamações não       |\n" +
+             "|   pararam de chegar. Michael está frustrado e a equipe está        |\n" +
+             "|   desanimada com o resultado. Infelizmente, essa derrota encerra   |\n" +
+             "|   sua jornada. O jogo chegou ao fim, e você terá que se esforçar   |\n" +
+             "|   mais na próxima vez para alcançar o sucesso.                     |\n" +
+             " \\                                                                 /\n" +
+             "  \\_______________________________________________________________/\n\n";
+
+
+
 }
