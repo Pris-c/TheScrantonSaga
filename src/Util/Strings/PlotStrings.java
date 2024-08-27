17f7065d4fa309f5ff473830e20c57d246dff709
Priscila <priscila.campoos@yahoo.com.br>
2024-08-27 19:38:58 +0100

    Add championMessage, loserMessage and title

--- src/Util/Strings/PlotStrings.java
+++ src/Util/Strings/PlotStrings.java
@@ -2,6 +2,23 @@ package Util.Strings;
 
 public class PlotStrings {
 
+    public static String title =
+                    "\n" +
+                    "\t\t\t\t\t████████╗██╗  ██╗███████╗\n" +
+                    "\t\t\t\t\t╚══██╔══╝██║  ██║██╔════╝\n" +
+                    "\t\t\t\t\t   ██║   ███████║█████╗  \n" +
+                    "\t\t\t\t\t   ██║   ██╔══██║██╔══╝  \n" +
+                    "\t\t\t\t\t   ██║   ██║  ██║███████╗\n" +
+                    "\t\t\t\t\t   ╚═╝   ╚═╝  ╚═╝╚══════╝\n" +
+                    "\n" +
+                    "\n" +
+                    "███████╗ ██████╗██████╗  █████╗ ███╗   ██╗████████╗ ██████╗ ███╗   ██╗    ███████╗ █████╗  ██████╗  █████╗ \n" +
+                    "██╔════╝██╔════╝██╔══██╗██╔══██╗████╗  ██║╚══██╔══╝██╔═══██╗████╗  ██║    ██╔════╝██╔══██╗██╔════╝ ██╔══██╗\n" +
+                    "███████╗██║     ██████╔╝███████║██╔██╗ ██║   ██║   ██║   ██║██╔██╗ ██║    ███████╗███████║██║  ███╗███████║\n" +
+                    "╚════██║██║     ██╔══██╗██╔══██║██║╚██╗██║   ██║   ██║   ██║██║╚██╗██║    ╚════██║██╔══██║██║   ██║██╔══██║\n" +
+                    "███████║╚██████╗██║  ██║██║  ██║██║ ╚████║   ██║   ╚██████╔╝██║ ╚████║    ███████║██║  ██║╚██████╔╝██║  ██║\n" +
+                    "╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚═╝  ╚═══╝    ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝\n";
+
     public static String initMessage =
                     "   _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.__.-.\n" +
                     " ,'_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._`..-\n" +
@@ -17,9 +34,9 @@ public class PlotStrings {
                     " ) )  cada dia é uma nova aventura.                           ( (\n" +
                     "( (                                                            ) )\n" +
                     " ) )  Prepare-se para navegar pelas loucuras do escritório.   ( (\n" +
-                    "( (  Lembre-se: a sobrevivência aqui não depende apenas de     ) )\n" +
+                    "( (   Lembre-se: a sobrevivência aqui não depende apenas de    ) )\n" +
                     " ) )  habilidades, mas também de quem você conhece e como     ( (\n" +
-                    "( (  você joga suas cartas. Boa sorte, porque no fim do dia,   ) )\n" +
+                    "( (   você joga suas cartas. Boa sorte, porque no fim do dia,  ) )\n" +
                     " ) )  só os mais astutos saem vitoriosos!                     ( (\n" +
                     "( (                                                            ) )\n" +
                     " ) )  \033[1mVocê está pronto para começar?\033[0m                          ( (\n" +
@@ -36,4 +53,65 @@ public class PlotStrings {
             "\t\t|     1 ponto de vida  = 1 moeda        |\n" +
             "\t\t+--------------------------------------+\n\n";
 
+
+    public static String championMessage =
+                    "                                  ___________\n" +
+                    "                             .---'::'        `---.\n" +
+                    "                            (::::::'              )\n" +
+                    "                            |`-----._______.-----'|\n" +
+                    "                            |              :::::::|\n" +
+                    "                           .|               ::::::!-.\n" +
+                    "                           \\|               :::::/|/\n" +
+                    "                            |               ::::::|\n" +
+                    "                            |                :::::|\n" +
+                    "                            |                :::::|\n" +
+                    "                            |               ::::::|\n" +
+                    "                            |              .::::::|\n" +
+                    "                            J              :::::::F\n" +
+                    "                             \\            :::::::/\n" +
+                    "                              `.        .:::::::'\n" +
+                    "                                `-._  .:::::::-'\n" +
+                    " ____________________________________|  \"\"\"|\"____________________________________________\n" +
+                    "|                                    |  :::|                                             |\n" +
+                    "|                                    F   ::J                                             |\n" +
+                    "|                                   /     ::\\                                            |\n" +
+                    "|                              __.-'      :::`-.__                                       |\n" +
+                    "|                             (_           ::::::_)                                      |\n" +
+                    "|                                                                                        |\n" +
+                    "|      Parabéns!                                                                         |\n" +
+                    "|      Você sobreviveu a todos os desafios e conquistou seu lugar na Dunder Mifflin.     |\n" +
+                    "|      Sua astúcia, determinação e habilidade para navegar pelo caos do escritório       |\n" +
+                    "|      fizeram de você um verdadeiro herói!                                              |\n" +
+                    "|      Agora, você pode finalmente relaxar e aproveitar a vitória – você merece.         |\n" +
+                    "|      Até a próxima aventura!                                                           |\n" +
+                    "|                                                                                        |\n" +
+                    " ----------------------------------------------------------------------------------------'\n";
+
+    public static String loserMessage =
+
+            "                               ⡴⠒⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠉⠳⡆⠀\n" +
+                    "                               ⣇⠰⠉⢙⡄⠀⠀⣴⠖⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠁⠙⡆\n" +
+                    "                               ⠘⡇⢠⠞⠉⠙⣾⠃⢀⡼⠀⠀⠀⠀⠀⠀⠀⢀⣼⡀⠄⢷⣄⣀⠀⠀⠀⠀⠀⠀⠀⠰⠒⠲⡄⠀⣏⣆⣀⡍\n" +
+                    "                               ⠀⢠⡏⠀⡤⠒⠃⠀⡜⠀⠀⠀⠀⠀⢀⣴⠾⠛⡁⠀⠀⢀⣈⡉⠙⠳⣤⡀⠀⠀⠀⠘⣆⠀⣇⡼⢋⠀⠀⢱\n" +
+                    "                               ⠀⠘⣇⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⡴⢋⡣⠊⡩⠋⠀⠀⠀⠣⡉⠲⣄⠀⠙⢆⠀⠀⠀⣸⠀⢉⠀⢀⠿⠀⢸\n" +
+                    "                               ⠀⠀⠸⡄⠀⠈⢳⣄⡇⠀⠀⢀⡞⠀⠈⠀⢀⣴⣾⣿⣿⣿⣿⣦⡀⠀⠀⠀⠈⢧⠀⠀⢳⣰⠁⠀⠀⠀⣠⠃\n" +
+                    "                               ⠀⠀⠀⠘⢄⣀⣸⠃⠀⠀⠀⡸⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠈⣇⠀⠀⠙⢄⣀⠤⠚⠁⠀\n" +
+                    "                               ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                    "                               ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                    " _______________________________________⠀⠀⡇⠀⢰⣿⣿⣿⡿⠛⠁⠀⠉⠛⢿⣿⣿⣿⣧⠀⠀⣼ _________________________________\n" +
+                    "|                               ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⣸⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡀⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀                        |\n" +
+                    "|                               ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇⠹⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠁⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                        |\n" +
+                    "|                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣤⣞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⣀⣠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                        |\n" +
+                    "|                              ⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠙⠲⢤⣀⣀⠀⢀⣀⣀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                         |\n" +
+                    "|                                                                                               |\n" +
+                    "|          Parece que o escritório de Dunder Mifflin foi demais para você desta vez.            |\n" +
+                    "|          Apesar de seus esforços, os desafios se mostraram insuperáveis. Mas não desanime     |\n" +
+                    "|          – nem todos conseguem ser um herói na primeira tentativa.                            |\n" +
+                    "|          Respire fundo, recomece e, quem sabe, da próxima vez, a história tenha um            |\n" +
+                    "|          final diferente.                                                                     |\n" +
+                    "|                                                                                               |\n" +
+                    " -----------------------------------------------------------------------------------------------'\n";
+
 }
+
+
