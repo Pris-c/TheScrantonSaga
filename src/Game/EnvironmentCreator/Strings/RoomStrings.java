510744c0fc7b4be4e30d4e4d94b4230b2532c5ac
Priscila <priscila.campoos@yahoo.com.br>
2024-08-27 17:53:07 +0100

    refactor: update Strings border

--- src/Game/EnvironmentCreator/Strings/RoomStrings.java
+++ src/Game/EnvironmentCreator/Strings/RoomStrings.java
@@ -3,26 +3,37 @@ package Game.EnvironmentCreator.Strings;
 public class RoomStrings {
 
     public static String intRoom1 =
-            "   _______________________________________________________________\n" +
-                    "Você chegou na copa e descobriu que a máquina de vendas está agindo de forma estranha hoje.\n" +
-                    " Ao tentar comprar um item, você pode\n" +
-                    " ter a sorte de receber um bônus e aumentar seu HP e sua força sem custo!.\n" +
-                    " Mas cuidado: se a máquina falhar, você pode acabar sem nada e ainda ficar mais fraco\n " +
-                    "por não conseguir se alimentar. Você arrisca tentar a sorte ou busca outra forma de suprir\n " +
-                    "suas necessidades?\n" +
-                    "  \\_______________________________________________________________/\n\n";
+                    "   _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.__.-.\n" +
+                    " ,'_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._`.-..-\n" +
+                    "( (                                                                 ) )\n" +
+                    " ) )  Você chegou na copa e descobriu que a máquina de vendas está ( (\n" +
+                    "( (   agindo de forma estranha hoje. Ao tentar comprar um item,     ) )\n" +
+                    " ) )  você pode ter a sorte de receber um bônus e aumentar seu HP  ( (\n" +
+                    "( (   e sua força sem custo! Mas cuidado: se a máquina falhar,      ) )\n" +
+                    " ) )  você pode acabar sem nada e ainda ficar mais fraco por não   ( (\n" +
+                    "( (   conseguir se alimentar. Você arrisca tentar a sorte ou busca   ) )\n" +
+                    " ) )  outra forma de suprir suas necessidades?                      ( (\n" +
+                    "( (                                                                 ) )\n" +
+                    "( (_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.) )\n" +
+                    " `._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._,.-.'\n";
 
 
     public static String optRoom1 =
             "Escolha uma opção:\n1 - Para tentar a sorte\n\033[3mDigite 0 para seguir em frente...\033[0m";
 
     public static String intRoom2 =
-            "   _______________________________________________________________\n" +
-                    "Enquanto procura algo para comer, você encontra uma marmita não identificada no refrigerador. \n" +
-                    "O alimento pode ser uma fonte inesperada de força extra sem gastar moedas, mas também pode\n" +
-                    " ser perigoso. Arriscar comer pode levar a uma intoxicação e perda de força e HP.\n" +
-                    "Você se atreve a experimentar essa refeição misteriosa?\n" +
-                    "  \\_______________________________________________________________/\n\n";
+                    "   _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._._.-.__.-.\n" +
+                    " ,'_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._`.-._`.-.-\n" +
+                    "( (                                                                    ) )\n" +
+                    " ) )  Enquanto procura algo para comer, você encontra uma marmita      ( (\n" +
+                    "( (   não identificada no refrigerador. O alimento pode ser uma         ) )\n" +
+                    " ) )  fonte inesperada de força extra sem gastar moedas, mas também    ( (\n" +
+                    "( (   pode ser perigoso. Arriscar comer pode levar a uma                ) )\n" +
+                    " ) )  intoxicação e perda de força e HP. Você se atreve a experimentar ( (\n" +
+                    "( (   essa refeição misteriosa?                                         ) )\n" +
+                    " ) )                                                                   ( (\n" +
+                    "( (_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._._.-._._.-.) )\n" +
+                    " `._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.._.-.._.-.-.'\n";
 
     public static String optRoom2 =
             "Escolha uma opção:\n1 - Para consumir a marmita\n\033[3mDigite 0 para seguir em frente...\033[0m";
@@ -31,20 +42,20 @@ public class RoomStrings {
 
 
     public static String intRoom3 =
-            "   _______________________________________________________________\n" +
-                    "  /                                                               \\\n" +
-                    " /                                                                 \\\n" +
-                    "|   Ao se aproximar, você percebe que a situação é mais complicada |\n" +
-                    "|   do que parecia. Dwight está em plena operação para capturar um |\n" +
-                    "|   morcego que invadiu o escritório! Ele está determinado a       |\n" +
-                    "|   capturá-lo com suas próprias mãos, mas a situação é caótica.   |\n" +
-                    "|                                                                 |\n" +
-                    "|   Você pode tentar ajudar Dwight a capturar o morcego e, se      |\n" +
-                    "|   tiver sucesso, ganhar pontos de vida e força extra. Mas        |\n" +
-                    "|   cuidado – se as coisas derem errado, você pode acabar se       |\n" +
-                    "|   machucando e sendo prejudicado.                                |\n" +
-                    " \\                                                               /\n" +
-                    "  \\_______________________________________________________________/\n\n";
+                    "   _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.\n" +
+                    " ,'_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._`..-\n" +
+                    "( (                                                                 ) )\n" +
+                    " ) )  Ao se aproximar, você percebe que a situação é mais          ( (\n" +
+                    "( (   complicada do que parecia. Dwight está em plena operação      ) )\n" +
+                    " ) )  para capturar um morcego que invadiu o escritório! Ele está  ( (\n" +
+                    "( (   determinado a capturá-lo com suas próprias mãos, mas a        ) )\n" +
+                    " ) )  situação é caótica. Você pode tentar ajudar Dwight a         ( (\n" +
+                    "( (   capturar o morcego e, se tiver sucesso, ganhar pontos de      ) )\n" +
+                    " ) )  vida e força extra. Mas cuidado – se as coisas derem errado, ( (\n" +
+                    "( (   você pode acabar se machucando e sendo prejudicado.           ) )\n" +
+                    " ) )                                                               ( (\n" +
+                    "( (_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-..-._.) )\n" +
+                    " `._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._,.-._,.-.'\n";
 
     public static String optRoom3 =
             "Escolha uma opção:\n1 - Para ajudar o Dwight\n\033[3mDigite 0 enter para seguir em frente...\033[0m";
