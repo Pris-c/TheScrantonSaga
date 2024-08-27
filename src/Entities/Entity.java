4763de3bc3765bda19403f2f46725dc0bb7c1bff
Priscila <priscila.campoos@yahoo.com.br>
2024-08-27 17:59:17 +0100

    refactor: change maxHp to class Hero

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -5,29 +5,21 @@ import static Util.Util.largeDetailFormat;
 public abstract class Entity {
 
     protected String name;
-    protected int maxHp;
     protected int hp;
     protected int strength;
     protected int gold;
 
-    public Entity(String name, int maxHp, int strength, int gold) {
+    public Entity(String name, int hp, int strength, int gold) {
         this.name = name;
-        this.maxHp = maxHp;
-        this.hp = maxHp;
+        this.hp = hp;
         this.strength = strength;
         this.gold = gold;
     }
 
     protected void showDetails() {
-        System.out.printf(largeDetailFormat, "Hp máximo", this.maxHp);
         System.out.printf(largeDetailFormat, "Hp atual", this.hp);
         System.out.printf(largeDetailFormat, "Força", this.strength);
         System.out.printf(largeDetailFormat, "Moedas", this.gold);
-    }
-
-    public void addHp(int hp) {
-        int newHp = this.hp + hp;
-        this.hp = Math.min(newHp, this.maxHp);
     }
 
     public int decrementHp(int hp) {
@@ -38,10 +30,6 @@ public abstract class Entity {
 
     public String getName() {
         return name;
-    }
-
-    public int getMaxHp() {
-        return maxHp;
     }
 
     public int getHp() {
