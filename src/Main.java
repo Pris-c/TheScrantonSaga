9472315b7c90c2c084f7d5880a898e5a36551dcf
Priscila <priscila.campoos@yahoo.com.br>
2024-08-28 14:16:37 +0100

    feat: add method to run the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Game.ScrantonSaga;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ScrantonSaga game = new ScrantonSaga();
+        game.initGame();
+
     }
 }
