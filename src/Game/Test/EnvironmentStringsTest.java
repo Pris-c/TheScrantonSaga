81a4b97e6ff3d9bebda07a09ae40de9f9ca63211
Priscila <priscila.campoos@yahoo.com.br>
2024-08-28 14:13:30 +0100

    test: create EnvironmentStringsTest

---
+++ src/Game/Test/EnvironmentStringsTest.java
@@ -0,0 +1,147 @@
+package Game.Test;
+
+import static Game.EnvironmentCreator.Strings.ChallengeStrings.*;
+import static Game.EnvironmentCreator.Strings.ChallengeStrings.victoryMessageC2;
+import static Game.EnvironmentCreator.Strings.NodeStrings.*;
+import static Game.EnvironmentCreator.Strings.RoomStrings.*;
+import static Game.EnvironmentCreator.Strings.ShopkeeperStrings.*;
+import static Util.Strings.PlotStrings.*;
+
+public class EnvironmentStringsTest {
+
+    public static void main(String[] args) {
+
+        System.out.print(title);
+        System.out.println();
+
+        System.out.print(initMessage);
+        System.out.println();
+
+        System.out.print(intChallenge1);
+        System.out.println();
+
+        System.out.print(victoryMessageC1);
+        System.out.println();
+
+        System.out.print(defeatMessageC1);
+        System.out.println();
+
+        System.out.print(intChallenge2);
+        System.out.println();
+
+        System.out.print(victoryMessageC2);
+        System.out.println();
+
+        System.out.print(defeatMessageC2);
+        System.out.println();
+
+        System.out.print(intChallenge3);
+        System.out.println();
+
+        System.out.print(victoryMessageC3);
+        System.out.println();
+
+        System.out.print(defeatMessageC3);
+        System.out.println();
+
+        System.out.print(intChallenge4);
+        System.out.println();
+
+        System.out.print(victoryMessageC4);
+        System.out.println();
+
+        System.out.print(defeatMessageC4);
+        System.out.println();
+
+        System.out.print(intChallenge5);
+        System.out.println();
+
+        System.out.print(victoryMessageC5);
+        System.out.println();
+
+        System.out.print(defeatMessageC5);
+        System.out.println();
+
+        System.out.print(intChallenge6);
+        System.out.println();
+
+        System.out.print(victoryMessageC6);
+        System.out.println();
+
+        System.out.print(defeatMessageC6);
+        System.out.println();
+
+        System.out.print(intChallenge7);
+        System.out.println();
+
+        System.out.print(victoryMessageC7);
+        System.out.println();
+
+        System.out.print(defeatMessageC7);
+        System.out.println();
+
+        System.out.print(intNode1);
+        System.out.println();
+
+        System.out.print(optNode1);
+        System.out.println();
+
+        System.out.print(intNode2);
+        System.out.println();
+
+        System.out.print(optNode2);
+        System.out.println();
+
+        System.out.print(intNode3);
+        System.out.println();
+
+        System.out.print(optNode3);
+        System.out.println();
+
+        System.out.print(intNode4);
+        System.out.println();
+
+        System.out.print(optNode4);
+        System.out.println();
+
+        System.out.print(intNode5);
+        System.out.println();
+
+        System.out.print(optNode5);
+        System.out.println();
+
+        System.out.print(intRoom1);
+        System.out.println();
+
+        System.out.print(optRoom1);
+        System.out.println();
+
+        System.out.print(intRoom2);
+        System.out.println();
+
+        System.out.print(optRoom2);
+        System.out.println();
+
+        System.out.print(intRoom3);
+        System.out.println();
+
+        System.out.print(optRoom3);
+        System.out.println();
+
+        System.out.print(intShopk1);
+        System.out.println();
+
+        System.out.print(intShopk2);
+        System.out.println();
+
+        System.out.print(intShopk3);
+        System.out.println();
+
+        System.out.print(championMessage);
+        System.out.println();
+
+        System.out.print(loserMessage);
+        System.out.println();
+
+    }
+}
