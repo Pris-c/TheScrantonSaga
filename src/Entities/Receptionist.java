88a4d7bf5784652947566d40ab5963a34a418b16
Priscila <priscila.campoos@yahoo.com.br>
2024-08-30 18:04:25 +0100

    doc: document class

--- src/Entities/Receptionist.java
+++ src/Entities/Receptionist.java
@@ -3,19 +3,34 @@ package Entities;
 import Util.Creator.ItemsCreator.CombatConsumableCreator;
 import Util.Creator.ItemsCreator.PotionCreator;
 import Util.Creator.ItemsCreator.WeaponCreator;
-import Util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static Util.Util.*;
+import static Util.Util.largeDetailFormat;
+import static Util.Util.largeTextFormat;
 
+/**
+ * Represents a specific type of Hero with unique characteristics.
+ * The Receptionist is equipped with a "Client Recommendation" weapon and starts with specific potions and combat consumables.
+ */
 public class Receptionist extends Hero {
 
+    /**
+     * Constructs a new Receptionist character with specified attributes.
+     *
+     * @param name     The name of the Receptionist.
+     * @param maxHp    The maximum hit points (HP) the Receptionist can keep.
+     * @param strenght The value of strength attribute, it influences attack power.
+     * @param gold     The initial amount of gold the recepcionist has.
+     */
     public Receptionist(String name, int maxHp, int strenght, int gold) {
         super(name, maxHp, strenght, gold, WeaponCreator.createClientRecommendation(), new ArrayList<>(List.of(PotionCreator.coffee(), CombatConsumableCreator.createDwightHelp())));
     }
 
+    /**
+     * Print to the console the details of the Receptionist hero.
+     */
     @Override
     public void showDetails() {
         System.out.println("+-----------------------------------------------------------------------------+");
@@ -26,18 +41,14 @@ public class Receptionist extends Hero {
 
     }
 
-    /*
-    attack(){
-        boolean special;
 
-        while(ngmmorre){
-            heroAttack(..)
-            enemyAttack(..)
-        }
-
-
+    /**
+     * Executes the Recepcionist's attack strategy during combat.
+     * The special instant attack can only be used once per combat.
+     *
+     * @param enemy the enemy that the Recepcionist is fighting against.
+     * @return true if the Recepcionist ends the fight with HP greater than 0; false otherwise.
      */
-
     @Override
     public boolean attack(Npc enemy) {
 
@@ -47,10 +58,10 @@ public class Receptionist extends Hero {
 
         do {
             int attack = super.heroAttack(enemy, specialAttack);
-            if (attack == 3){
+            if (attack == 3) {
                 specialAttack = false;
             }
-            if (enemy.getHp() <= 0){
+            if (enemy.getHp() <= 0) {
                 return true;
             }
 
