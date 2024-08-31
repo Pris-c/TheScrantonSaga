20f78ef78d8ed2b2c7b559e3c0bc379d2148cb79
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 19:32:05 +0100

    doc: document class

--- src/Game/GameEnvironment.java
+++ src/Game/GameEnvironment.java
@@ -2,26 +2,54 @@ package Game;
 
 import Entities.Hero;
 
+/**
+ * Represents an abstract game environment that provides playable scenarios.
+ * Subclasses must define specific types of Environments.
+ */
 public abstract class GameEnvironment {
 
     protected int id;
     protected String initialMessage;
-    protected GameEnvironment nextEnviroment;
+    protected GameEnvironment nextEnvironment;
     protected int nextNode;
 
-    public GameEnvironment(int id, String initialMessage, GameEnvironment nextEnviroment) {
+    /**
+     * Constructs a GameEnvironment that follows to a next environment
+     *
+     * @param id             The unique identifier of the environment
+     * @param initialMessage The message to be displayed in the beginning of the environment situation
+     * @param nextEnvironment The next environment to be called, when exists.
+     */
+    public GameEnvironment(int id, String initialMessage, GameEnvironment nextEnvironment) {
         this.id = id;
         this.initialMessage = initialMessage;
-        this.nextEnviroment = nextEnviroment;
+        this.nextEnvironment = nextEnvironment;
         this.nextNode = 0;
     }
 
+    /**
+     * Constructs the final GameEnvironment in a sequence.
+     *
+     * @param id             The unique identifier of the environment
+     * @param initialMessage The message to be displayed in the beginning of the environment situation
+     * @param nextNode       The id of the Node to be called in sequence. It must be 0 if it is the final game Environment.
+     */
     public GameEnvironment(int id, String initialMessage, int nextNode) {
         this.id = id;
         this.initialMessage = initialMessage;
         this.nextNode = nextNode;
-        this.nextEnviroment = null;
+        this.nextEnvironment = null;
     }
 
+    /**
+     * Executes the GameEnvironment behavior.
+     * It must be implemented by subclasses to define the specific scenario.
+     *
+     * @param hero The hero character interacting with the environment.
+     * @return An integer representing a game status:
+     *          -1 if the Hero lost the game in current environment
+     *          0 if the Environment is the last obstacle in the game
+     *          or the nextNode id if Hero passes successfully by the environment
+     */
     public abstract int run(Hero hero);
 }
