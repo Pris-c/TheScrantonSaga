a5e0788fabbdd1f9fd84b2170aa03cdb140d647d
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 13:20:57 +0100

    doc: document class

--- src/Items/CombatConsumable.java
+++ src/Items/CombatConsumable.java
@@ -2,15 +2,32 @@ package Items;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a Combat Consumable item in the game, extending the Consumable class.
+ * This item offers the Hero an instant Attack, witch replaces Hero's attack strength when used.
+ * A CombatConsumable adds to the Consumable the specific value of its instantAttack.
+ */
 public class CombatConsumable extends Consumable {
 
     private int instantAttack;
 
+    /**
+     * Constructs a CombatConsumable with specified attributes.
+     *
+     * @param name          The name of the Weapon.
+     * @param price         The value that the Hero must pay to obtain the item.
+     * @param allowedTo     A list of Hero types that can buy the item.
+     * @param instantAttack The value of the instant attack that will replace the Hero's attack value when used.
+     */
     public CombatConsumable(String name, int price, ArrayList<String> allowedTo, int instantAttack) {
         super(name, price, allowedTo);
         this.instantAttack = instantAttack;
     }
 
+    /**
+     * Prints to the console detailed information about the CombatConsumable, as the price and the
+     * instant attack value.
+     */
     @Override
     public void showDetails() {
         String format = "| %-20s %-10s |\n";
@@ -26,6 +43,10 @@ public class CombatConsumable extends Consumable {
 
     }
 
+    /**
+     * Prints the consumable's name and corresponding instant attack value to the console.
+     * This method is useful for showing the combat potential of the item.
+     */
     public void showAttackInfo() {
         String headerFormat = "| %-31s |\n";
         System.out.printf(headerFormat, super.name.toUpperCase());
@@ -36,6 +57,12 @@ public class CombatConsumable extends Consumable {
         return instantAttack;
     }
 
+    /**
+     * Provides a short description of the CombatConsumable,
+     * witch can be used for quick reference.
+     *
+     * @return A short description of the consumable, including its name.
+     */
     @Override
     public String shortDescription() {
         return "Ajuda extra: " + super.name;
