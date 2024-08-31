b3241687474611c1198fe0f707bde94928884a0e
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 20:29:46 +0100

    doc: document class

--- src/Game/Room.java
+++ src/Game/Room.java
@@ -7,6 +7,10 @@ import java.util.Random;
 import static Util.Strings.EnvironmentStrings.RoomStrings.defeatRoom;
 import static Util.Util.*;
 
+/**
+ * Represents a specific playable environment, extending the GameEnvironment class.
+ * In this environment, the player can gain or lose strength and/or HP points based on a random value.
+ */
 public class Room extends GameEnvironment {
 
     private String questionToPlayer;
@@ -16,8 +20,21 @@ public class Room extends GameEnvironment {
     private int hpIncrement;
     private int hpDecrement;
 
-    public Room(int id, String initialMessage, String questionToPlayer, int percentLuck, int strengthIncrement, int strengthDecrement, int hpIncrement, int hpDecrement, GameEnvironment nextEnviroment) {
-        super(id, initialMessage, nextEnviroment);
+    /**
+     * Constructs a Room instance with the specified attributes.
+     *
+     * @param id                The unique identifier of the Room
+     * @param initialMessage    The message to be displayed in the beginning of the Room situation
+     * @param questionToPlayer  The question to be presented to the player, asking for the decision to run the Room or continuous the game.
+     * @param percentLuck       The percentage chance that the player gains advantages running the Room
+     * @param strengthIncrement The value of the strength increment when the player succeeds
+     * @param strengthDecrement The value of the strength decrease when the player has bad luck
+     * @param hpIncrement       The value of the hp increment when the player succeeds
+     * @param hpDecrement       The value of the hp decrease when the player has bad luck
+     * @param nextEnvironment   The next environment to be called
+     */
+    public Room(int id, String initialMessage, String questionToPlayer, int percentLuck, int strengthIncrement, int strengthDecrement, int hpIncrement, int hpDecrement, GameEnvironment nextEnvironment) {
+        super(id, initialMessage, nextEnvironment);
         this.questionToPlayer = questionToPlayer;
         this.percentLuck = percentLuck;
         this.strengthIncrement = strengthIncrement;
@@ -26,6 +43,19 @@ public class Room extends GameEnvironment {
         this.hpDecrement = hpDecrement;
     }
 
+    /**
+     * Constructs a Room instance with the specified attributes.
+     *
+     * @param id                The unique identifier of the Room
+     * @param initialMessage    The message to be displayed in the beginning of the Room situation
+     * @param questionToPlayer  The question to be presented to the player, asking for the decision to run the Room or continuous the game.
+     * @param percentLuck       The percentage chance that the player gains advantages running the Room
+     * @param strengthIncrement The value of the strength increment when the player succeeds
+     * @param strengthDecrement The value of the strength decrease when the player has bad luck
+     * @param hpIncrement       The value of the hp increment when the player succeeds
+     * @param hpDecrement       The value of the hp decrease when the player has bad luck
+     * @param nextNode          The id of the Node to be called in sequence. It must be 0 if it is the final game Environment.
+     */
     public Room(int id, String initialMessage, String questionToPlayer, int percentLuck, int strengthIncrement, int strengthDecrement, int hpIncrement, int hpDecrement, int nextNode) {
         super(id, initialMessage, nextNode);
         this.questionToPlayer = questionToPlayer;
@@ -36,6 +66,16 @@ public class Room extends GameEnvironment {
         this.hpDecrement = hpDecrement;
     }
 
+    /**
+     * Runs the room scenario.
+     * The player can choose to test their luck or just continue the game.
+     *
+     * @param hero The hero interacting with the room.
+     * @return An integer representing a game status:
+     *          -1 if the Hero loses the game in current challenge
+     *          0 if the Room is the last obstacle in the game
+     *          or the nextNode id if Hero passes successfully by the room
+     */
     @Override
     public int run(Hero hero) {
         Random rd = new Random();
@@ -82,8 +122,8 @@ public class Room extends GameEnvironment {
             }
 
             cleanScreen();
-            if (this.nextEnviroment != null) {
-                return this.nextEnviroment.run(hero);
+            if (this.nextEnvironment != null) {
+                return this.nextEnvironment.run(hero);
             }
         }
         return this.nextNode;
