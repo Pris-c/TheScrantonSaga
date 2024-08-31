4cafc63ba02e241c05d3ffd9cb55fb2aa9fee5f7
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 12:27:39 +0100

    doc: document class

--- src/Items/ItemHero.java
+++ src/Items/ItemHero.java
@@ -2,12 +2,24 @@ package Items;
 
 import java.util.ArrayList;
 
+/**
+ * Represents an abstract item in the game, that can be used by certain types of Heroes.
+ * It has the attributes name, price, and allowedTo, an ArrayList to define witch Hero can use each item.
+ * Subclasses must define specific types of ItemHero.
+ */
 public abstract class ItemHero {
 
     protected String name;
     protected int price;
     protected ArrayList<String> allowedTo;
 
+    /**
+     * Constructs an ItemHero with specified attributes.
+     *
+     * @param name The name of the Item.
+     * @param price The value that the Hero must pay to obtain the item.
+     * @param allowedTo A list of Hero types that can buy the item.
+     */
     public ItemHero(String name, int price, ArrayList<String> allowedTo) {
         this.name = name;
         this.price = price;
@@ -26,5 +38,8 @@ public abstract class ItemHero {
         return allowedTo;
     }
 
+    /**
+     * Abstract method to be implemented by subclasses to displays the details of the item.
+     */
     public abstract void showDetails();
 }
