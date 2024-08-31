47c1643f12b212ae45a0f67924547b65ead8c47f
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 20:06:27 +0100

    doc: document class

--- src/Game/Node.java
+++ src/Game/Node.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import static Util.Util.cleanScreen;
 import static Util.Util.readAndValidateInput;
 
+/**
+ * Represents a point of decision in the game where the player chooses a path to go.
+ */
 public class Node {
 
     private int id;
@@ -14,6 +17,14 @@ public class Node {
     private String optionsMessage;
     private ArrayList<GameEnvironment> destines;
 
+    /**
+     * Constructs a Node with specified attributes.
+     *
+     * @param id             The unique identifier of the node
+     * @param initialMessage The message to be displayed to presents the scenario of choices to the player.
+     * @param optionsMessage The message to present the options to the player
+     * @param destines       A list of possible GameEnvironments to move on.
+     */
     public Node(int id, String initialMessage, String optionsMessage, ArrayList<GameEnvironment> destines) {
         this.id = id;
         this.initialMessage = initialMessage;
@@ -21,6 +32,16 @@ public class Node {
         this.destines = destines;
     }
 
+
+    /**
+     * Prints the orientation messages to the console, read the user option and processes the next environment.
+     *
+     * @param hero The hero player
+     * @return An integer representing a game status:
+     *          -1 if the Hero loses the game during the environments of the current Node
+     *          0 if the Node is the last one in the game.
+     *          or the nextNode id if Hero passes successfully by the environments.
+     */
     public int run(Hero hero){
         System.out.println(this.initialMessage);
         int option = readAndValidateInput(this.optionsMessage, 1, this.destines.size());
