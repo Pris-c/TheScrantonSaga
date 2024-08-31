0251bec0873895f4492123f280749a1c303281bf
Priscila <priscila.campoos@yahoo.com.br>
2024-08-31 20:46:01 +0100

    doc: document class

--- src/Game/Shopkeeper.java
+++ src/Game/Shopkeeper.java
@@ -12,94 +12,51 @@ import java.util.Random;
 
 import static Util.Util.*;
 
+/**
+ * Represents a specific playable environment, extending the GameEnvironment class.
+ * The Shopkeeper is the environment where the player can buy weapons, combat consumables or potions to improve their
+ * abilities.
+ */
 public class Shopkeeper extends GameEnvironment{
 
     private static ArrayList<ItemHero> store;
     private static boolean initInstance;
 
-    public Shopkeeper(int id, String initialMessage, GameEnvironment nextEnviroment) {
-        super(id, initialMessage, nextEnviroment);
+    /**
+     * Constructs a Shopkeeper environment
+     *
+     * @param id             The unique identifier of the environment
+     * @param initialMessage The message to be displayed in the beginning of the environment situation
+     * @param nextEnvironment The next environment to be called
+     */
+    public Shopkeeper(int id, String initialMessage, GameEnvironment nextEnvironment) {
+        super(id, initialMessage, nextEnvironment);
     }
 
+    /**
+     * Constructs a Shopkeeper environment
+     *
+     * @param id             The unique identifier of the environment
+     * @param initialMessage The message to be displayed in the beginning of the environment situation
+     * @param nextNode       The id of the Node to be called in sequence. It must be 0 if it is the final game Environment.
+     */
     public Shopkeeper(int id, String initialMessage, int nextNode) {
         super(id, initialMessage, nextNode);
         if (!initInstance){
             initInstance = true;
-            store = fillInventory();
+            store = fillStore();
         }
     }
 
-    /*    public Shopkeeper(int id, String initialMessage) {
-        super(id, initialMessage);
-        if (!initInstance){
-            initInstance = true;
-            store = fillInventory();
-        }
-    }*/
-
-/*    @Override
-    public boolean run(Hero hero) {
-        System.out.println(this.initialMessage);
-        readContinue();
-
-        Random random = new Random();
-        ArrayList<ItemHero> heroItems = this.filterDistinctItemsToHero(hero);
-        ArrayList<ItemHero> itensToSell = new ArrayList<>();
-        if (heroItems.size() <= 10) {
-            itensToSell = heroItems;
-        } else {
-            while (itensToSell.size() < 10) {
-                ItemHero item = heroItems.get(random.nextInt(heroItems.size()));
-                itensToSell.add(item);
-                heroItems.remove(item);
-            }
-        }
-
-        int option;
-        int nItem = -1;
-        while (nItem != 0) {
-
-            cleanScreen();
-            printItemsToSell(itensToSell);
-            System.out.println("Você tem " + hero.getGold() + " moedas.");
-            nItem = Util.readAndValidateInput("Informe o número do item para ver os detalhes:\n\033[3mPara sair, digite 0. \033[0m", 0, itensToSell.size());
-
-            if (nItem > 0) {
-
-                cleanScreen();
-                nItem--;
-                ItemHero item = itensToSell.get(nItem);
-                nItem++;
-                item.showDetails();
-                option = Util.readAndValidateInput("Digite 1 para comprar o item.\n\033[3mPara voltar, digite 0. \033[0m", 0, 1);
-                cleanScreen();
-
-                if (option == 1) {
-                    if (this.sell(hero, item)) {
-                        itensToSell.remove(item);
-                        option = readAndValidateInput("Digite 1 para consultar as informações da personagem\n\033[3mDigite 0 para retornar à loja..\033[0m", 0, 1);
-                        if (option == 1){
-                            cleanScreen();
-                            hero.showDetails();
-                            readContinue("Pressione enter para voltar à loja..");
-                        }
-                    }
-                }
-            }
-        }
-        cleanScreen();
-
-        if (hero.checkPotionAvailability()){
-            int consumeOption = -1;
-            consumeOption = readAndValidateInput("Deseja consumir algo agora?\n1 - Sim, me mostre minhas opções\n0 - Não, quero seguir em frente", 0, 1);
-            cleanScreen();
-            if (consumeOption == 1){
-                hero.offerPotionsIfAvailable();
-            }
-        }
-        return true;
-    }*/
-
+    /**
+     * Runs the Shopkeeper environment, allowing the hero to view and buy items.
+     * The player can also choose to consume potions if they have any.
+     *
+     * @param hero The hero interacting with the Shopkeeper.
+     * @return An integer representing the game status:
+     *         0 if the Shopkeeper is the last environment.
+     *         or the id of the next node.
+     */
     @Override
     public int run(Hero hero) {
         System.out.println(this.initialMessage);
@@ -153,7 +110,7 @@ public class Shopkeeper extends GameEnvironment{
         cleanScreen();
 
         if (hero.checkPotionAvailability()){
-            int consumeOption = -1;
+            int consumeOption;
             consumeOption = readAndValidateInput("Deseja consumir algo agora?\n1 - Sim, me mostre minhas opções\n0 - Não, quero seguir em frente", 0, 1);
             cleanScreen();
             if (consumeOption == 1){
@@ -161,13 +118,21 @@ public class Shopkeeper extends GameEnvironment{
             }
         }
 
-        if (this.nextEnviroment != null){
-            return this.nextEnviroment.run(hero);
+        if (this.nextEnvironment != null){
+            return this.nextEnvironment.run(hero);
 
         }
         return nextNode;
     }
 
+    /**
+     * Makes an item sale to the hero
+     * Remove gold of hero and add the item their inventory
+     *
+     * @param hero The hero character purchasing the item.
+     * @param item The item bought.
+     * @return True if the sale was successful, false otherwise.
+     */
     private boolean sell(Hero hero, ItemHero item) {
         if (hero.getGold() < item.getPrice()) {
             System.out.println("Desculpe, você não tem moedas suficientes para realizar essa compra.\n");
@@ -194,10 +159,16 @@ public class Shopkeeper extends GameEnvironment{
         }
     }
 
+    /**
+     * Filters and returns distinct items that the hero is allowed to have based on their class.
+     *
+     * @param hero The hero whose allowed items must be filtered.
+     * @return A list of distinct items available for the hero.
+     */
     private ArrayList<ItemHero> filterDistinctItemsToHero(Hero hero) {
         ArrayList<ItemHero> heroItems = new ArrayList<>();
         String heroClass = hero.getClass().getSimpleName();
-        for (ItemHero item : this.store) {
+        for (ItemHero item : store) {
             if (item.getAllowedTo().contains(heroClass)) {
                 heroItems.add(item);
             }
@@ -205,6 +176,11 @@ public class Shopkeeper extends GameEnvironment{
         return new ArrayList<>(heroItems.stream().distinct().toList());
     }
 
+    /**
+     * Prints to the console the items available in the shop.
+     *
+     * @param itensToSell The list of available items for sale.
+     */
     private void printItemsToSell(ArrayList<ItemHero> itensToSell) {
         String format = "| %-3s | %-20s | %-30s | %-7s |\n";
 
@@ -235,7 +211,12 @@ public class Shopkeeper extends GameEnvironment{
     }
 
 
-    private ArrayList<ItemHero> fillInventory(){
+    /**
+     * Initializes the store with all game items.
+     *
+     * @return The list of items available in the store.
+     */
+    private ArrayList<ItemHero> fillStore(){
         ArrayList<Weapon> weapons = WeaponCreator.initWeapons();
         ArrayList<CombatConsumable> combatConsumables = CombatConsumableCreator.getCombatConsumables();
         ArrayList<Potion> potions = PotionCreator.initPotions();
